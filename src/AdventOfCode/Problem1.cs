bfbb04d54cdebba19bc73a5c6be4ed0ffaf5d322
Jens Andersson <jens.andersson5@gmail.com>
2022-12-01 07:01:35 +0100

    Problem 1

---
+++ src/AdventOfCode/Problem1.cs
@@ -0,0 +1,24 @@
+﻿namespace AdventOfCode;
+
+internal class Problem1
+{
+    public static int Solve(ReadOnlySpan<string> input)
+    {
+        var maxCalories = 0;
+        var calories = 0;
+
+        foreach (var ina in input)
+        {
+            if (ina.Length == 0)
+            {
+                maxCalories = Math.Max(calories, maxCalories);
+                calories = 0;
+            }
+            else
+            {
+                calories += int.Parse(ina);
+            }
+        }
+        return maxCalories;
+    }
+}
--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode;
+
+// Problem  1
+{
+    var input = ReadInput(1);
+    var result = Problem1.Solve(input);
+    Console.WriteLine($"Result for problem1: {result}");
+}
+
+static string[] ReadInput(int problemNumber)
+{
+    const string basePath = @"..\..\..\..\..\inputs";
+
+    var path = Path.GetFullPath(Path.Combine(basePath, $"problem_{problemNumber}.txt"));
+    Console.WriteLine($"Reading input from {path}");
+    if (!File.Exists(path))
+    {
+        Console.Error.WriteLine($"File {path} does not exist.");
+        throw new InvalidOperationException("File does not exist.");
+    }
+    return File.ReadAllLines(path);
+
+}
