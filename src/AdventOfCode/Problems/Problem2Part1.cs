f0e505361f1f11ada3bac27825e3775945a62245
Jens Andersson <jens.andersson5@gmail.com>
2022-12-02 08:22:17 +0100

    Add naive implementation for Problem2

--- src/AdventOfCode/Problems/Problem2Part1.cs
+++ src/AdventOfCode/Problems/Problem2Part1.cs
@@ -1,5 +1,15 @@
 ﻿namespace AdventOfCode.Problems;
 
+// Rock A, X 2 point
+// Paper B, Y 1 point
+// Scissor C, Z 3 point
+
+file enum PlayerChoice
+{
+    Rock = 1,
+    Paper = 2,
+    Scissor = 3
+}
 internal class Problem2Part1 : IProblem
 {
     public static int Id => 2;
@@ -11,6 +21,43 @@ internal class Problem2Part1 : IProblem
 
     public static int SolveNaive(ReadOnlySpan<string> input)
     {
-        return -1;
+        var myScore = 0;
+        
+        foreach (var ina in input)
+        {
+            var values = ina.Split(" ", 2);
+            var opponent = values[0][0] switch
+            {
+                'A' => PlayerChoice.Rock,
+                'B' => PlayerChoice.Paper,
+                'C' => PlayerChoice.Scissor,
+            };
+            var me = values[1][0] switch
+            {
+                'X' => PlayerChoice.Rock,
+                'Y' => PlayerChoice.Paper,
+                'Z' => PlayerChoice.Scissor,
+            };
+
+            // Tie
+            if (opponent == me)
+            {
+                myScore += (int)me + 3;
+            }
+            // Win
+            else if (
+                (opponent is PlayerChoice.Paper && me is PlayerChoice.Scissor) ||
+                (opponent is PlayerChoice.Scissor && me is PlayerChoice.Rock) ||
+                (opponent is PlayerChoice.Rock && me is PlayerChoice.Paper)
+                )
+            {
+                myScore += (int)me + 6;
+            }
+            else
+            {
+                myScore += (int)me;
+            }
+        }
+        return myScore;
     }
 }
